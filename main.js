7b9c15831dd942410067ee1148e1bdc3436360bf
--global <--global>
2023-12-15 00:10:26 +0300

    Lesson 4

---
+++ main.js
@@ -0,0 +1,221 @@
+// For 1
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+// let sum = 0;
+
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+//   sum += 1;
+// }
+
+// console.log(sum, " son qatnashdi");
+
+// For 2
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+// a -= 1;
+// b += 1;
+// let sum = 0;
+
+// for (let i = a; i >= b; i--) {
+//   console.log(i);
+//   sum += 1;
+// }
+
+// console.log(sum, " son qatnashdi");
+
+// For 3
+
+// let k = +prompt("Konfet narxini kiriting");
+
+// for (let i = 1; i <= 10; i += 1) {
+//   console.log(i * k);
+// }
+
+// For 4
+
+// let k = +prompt("Konfet narxini kiriting");
+
+// for (let i = 10; i <= 20; i += 2) {
+//   console.log((k * i) / 10);
+// }
+
+// For 5
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+// let sum = 0;
+
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+//   sum += i;
+// }
+
+// console.log(sum, " sonlar yig`indisi");
+
+// For 6
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+// let sum = 1;
+
+// for (let i = a; i <= b; i++) {
+//   console.log(i);
+//   sum *= i;
+// }
+
+// console.log(sum, " sonlar ko`paytmasi");
+
+// For 7
+
+// let a = +prompt("a ni kiriting");
+// let b = +prompt("b ni kiriting");
+// let sum = 0;
+
+// for (let i = a; i <= b; i++) {
+//   console.log(i ** 2);
+//   sum += i ** 2;
+// }
+
+// console.log(sum, " sonlar kvadrati yig`indisi");
+
+// For 8
+
+// let n = +prompt("n ni kiriting");
+
+// for (let i = 1 / 2; i <= 1 / n; i++) {
+//   console.log(i);
+// }
+
+// For 9
+
+// let n = +prompt("n ni kiriting");
+
+// for (let i = 1.1; i <= n; i += 0.1) {
+//   console.log(i);
+// }
+
+// For 10
+
+// let n = +prompt("n ni kiriting");
+// let sum = 0;
+
+// for (let i = 1; i <= 2 * n - 1; i += 2) {
+//   console.log(i ** 2);
+//   sum += i ** 2;
+// }
+
+// console.log(sum);
+
+// For 11
+
+// let n = 5;
+// let a = 10;
+// let mul = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   mul *= a;
+// }
+// console.log(mul);
+
+// For 12
+
+// let n = +prompt("son");
+// let s = 2;
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(i, Math.pow(s, i));
+// }
+
+// For 13
+
+// let n = +prompt("son kirit");
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(n ** i);
+// }
+
+// For 14
+
+// let n = +prompt("son kirit");
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(i, "!");
+// }
+
+// For 15
+
+// let n = +prompt(" n son kirit");
+// let k = +prompt("k ni kirit");
+
+// for (let i = 1; i <= n; i++) {
+//   console.log(i ** k);
+// }
+
+// For 16
+
+// let n = +prompt("son ");
+// let sum = 0;
+
+// for (let i = 1; i <= n; i++) {
+//   sum += i ** n;
+// }
+
+// console.log(sum);
+
+// For 17
+
+// let a = 3;
+// let b = 8;
+
+// for (let i = a; i <= b; i++) {
+//   for (let k = 0; k <= i - a; k++) {
+//     console.log(i);
+//   }
+// }
+
+// For 18
+
+// let a = 4;
+// let sum = a;
+// let d = 1;
+
+// for (let i = 1; i <= a / 2; i++) {
+//   if (a % i == 0) {
+//     sum += i;
+//     d++;
+//     console.log(i);
+//   }
+// }
+
+// console.log(a);
+// console.log(sum);
+// console.log(d);
+
+// For 19
+
+// let a = 4;
+// let d = 0;
+
+// for (let i = 1; i <= a / 2; i++) {
+//   if (a % i == 0) {
+//     d++;
+//   }
+// }
+// if (d > 1) {
+//   console.log(a + " tub emas!");
+// } else {
+//   console.log(a + " tub!");
+// }
+
+// For 20
+
+// let A = +prompt("son kirit");
+// let d = "";
+
+// for (let i = 1; i <= A; i++) {
+//   d += i.toFixed();
+//   console.log(d);
+// }
